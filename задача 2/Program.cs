a2de6595dd02fa38a8c947ed48b3bbe63ac23cd2
Rustam <pkheshkhov0707@mail.ru>
2022-12-22 15:31:38 +0300

    вторая задача

---
+++ задача 2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+    else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+int summ = 0;
+int a = 1;
+int GETRESULT( int m, int n)
+{
+    
+    if(a < m)
+    {
+        a++;
+        GETRESULT(m,n);
+    }
+    
+    if(a >= m && a <= n)
+    {
+        summ = summ + a;
+        a++;
+        GETRESULT(m,n);
+
+    }
+    
+    return summ;
+}
+
+int m = GetNumber("введите m");
+int n = GetNumber("введите n");
+int result = GETRESULT(m,n);
+Console.WriteLine($"{result}");
